5946b4e64350393a70bd9a78a011fb70f15eb23e
anikett-rpx <anikett_c@rpxcorp.com>
2019-11-25 17:57:37 +0530

    f

---
+++ app/indices/article_index.rb
@@ -0,0 +1,8 @@
+class ArticleIndex
+  ThinkingSphinx::Index.define :book, :with => :active_record do
+    # fields
+    indexes title, :sortable => true
+    indexes author
+    indexes description
+  end
+end
